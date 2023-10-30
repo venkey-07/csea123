71a05d6755e85665150dd2bbe3e88730ebb8dabe
venkey-07 <141617276+venkey-07@users.noreply.github.com>
2023-10-30 12:10:29 +0530

    Update Newproject.java

--- Demo123/src/Package1/Newproject.java
+++ Demo123/src/Package1/Newproject.java
@@ -5,6 +5,8 @@ public class Newproject {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 				System.out.println("Hello world");
+				System.out.println("Hello world venkeuehaesbca ");
+
 
 		
 	}
